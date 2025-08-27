e6c2cee0f602112000720c4bccc3e2056441f8b2
Artem Vikulov <putk4k@mail.ru>
2025-08-27 11:46:39 +0300

    changed Convert func in service/service.go

--- internal/service/service.go
+++ internal/service/service.go
@@ -28,14 +28,18 @@ func Convert(input string) (string, error) {
 	if isMorse {
 		result := morse.ToText(input)
 		if strings.TrimSpace(result) == "" {
-			return input, nil
+			return "", errors.New("Ошибка конвертации из Морзе в Текст")
 		}
 		return result, nil
 	}
-
+	for _, ch := range input {
+		if _, ok := morse.DefaultMorse[unicode.ToUpper(ch)]; !ok {
+			return input, nil
+		}
+	}
 	result := morse.ToMorse(input)
 	if strings.TrimSpace(result) == "" {
-		return input, nil
+		return "", errors.New("Ошибка конвертации из Текста в Морзе")
 	}
 	return result, nil
 }
