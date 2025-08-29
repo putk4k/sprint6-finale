2ab4c2d2bfbc6e192ff963c3f88934f592cb143b
Artem Vikulov <putk4k@mail.ru>
2025-08-29 14:12:39 +0300

    added logging in handler.go and service.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -12,13 +13,13 @@ import (
 )
 
 func HandlerRoot(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("Получен запрос HandlerRoot")
 	http.ServeFile(w, r, "index.html")
 
 }
 
 func HandlerUpload(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("Получен запрос HandlerUpload")
 	if r.Method != "POST" {
 		http.Error(w, "Метод не поддерживается", http.StatusInternalServerError)
 		return
@@ -42,7 +43,7 @@ func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+	log.Println("Вызов функции конвертации")
 	converted, err := service.Convert(string(data))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -52,7 +53,7 @@ func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 	ext := filepath.Ext(header.Filename)
 
 	filename := fmt.Sprintf("result_%s%s", time.Now().UTC().Format("20060102_150405"), ext)
-
+	log.Println("Создание временного файла")
 	out, err := os.Create(filename)
 	if err != nil {
 		http.Error(w, "Ошибка при создании файла", http.StatusInternalServerError)
@@ -60,7 +61,7 @@ func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer out.Close()
-
+	log.Println("Начало записи в файл")
 	if _, err := out.WriteString(converted); err != nil {
 		http.Error(w, "Ошибка при записи в файл", http.StatusInternalServerError)
 		return
